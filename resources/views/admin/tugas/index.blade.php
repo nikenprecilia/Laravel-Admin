2c2b266dd9ecb3374730371cb347805b3f7e6828
Niken Precilia <nikenprecilia11@gmail.com>
2021-04-27 21:44:32 +0700

    task index berhasil

--- resources/views/admin/tugas/index.blade.php
+++ resources/views/admin/tugas/index.blade.php
@@ -50,8 +50,9 @@
                                         <tr>
                                             <th>#</th>
                                             <th>Nama</th>
+                                            <th>Kategori</th>
+                                            <th>Keterangan</th>
                                             <th>Status</th>
-                                            
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -59,8 +60,10 @@
                                         @foreach($data as $i=>$row)
                                         <tr>
                                             <td>{{++$i}}</td>
-                                            <td>{{$row->nama_kategori}}</td>
-                                            <td>{{$row->status_kategori}}</td>
+                                            <td>{{$row->nama_tugas}}</td>
+                                            <td>{{$row->id_kategori}}</td>
+                                            <td>{{$row->ket_tugas}}</td>
+                                            <td>{{$row->status_tugas}}</td>
                                         </tr>
                                         @endforeach
                                         <!-- <tr>
